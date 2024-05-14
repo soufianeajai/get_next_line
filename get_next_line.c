c33326df1d9c6a620c91cec862ca0814e6b48449
Soufiane Ajaite <soufiane@Soufianes-MacBook-Pro.local>
2024-05-14 22:15:41 +0100

    better version.

--- get_next_line.c
+++ get_next_line.c
@@ -12,69 +12,6 @@
 
 #include"get_next_line.h"
 
-
-// char	*get_next_line(int fd)
-// {
-// 	static char	buf[BUFFER_SIZE + 1];
-// 	char		*line;
-// 	char		*newline = 0;
-// 	int			countread;
-// 	int			to_copy;
-
-// 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buf, 0) < 0)
-// 		return (NULL);
-// 	line = ft_strdup(buf);
-// 	while (!(newline = ft_strchr(line, '\n')) && (countread = read(fd, buf, BUFFER_SIZE)))
-// 	{
-// 		buf[countread] = '\0';
-// 		line = ft_strjoin(line, buf);
-// 	}
-// 	if (ft_strlen(line) == 0)
-// 		return (free(line), NULL);
-// // case found newline
-// 	if (newline)
-// 	{
-// 		to_copy = newline - line;
-// 		ft_strcpy(buf, newline);
-// 	}
-// // case no newline
-// 	else
-// 	{
-// 		to_copy = ft_strlen(line);
-// 		buf[0] = '\0';
-// 	}
-// 	line[to_copy] = '\0';
-// 	return (line);
-// }
-
-
-
-
-
-/*
- int main()
- {	
-
- 	int fd;
- 	char	*line;
-	int i = 0;
-
- 	fd = open("test.txt", O_RDONLY);
- 	while (i>=0)
- 	{
- 		line = get_next_line(fd);
- 		if (line == NULL)
- 			break;
- 		printf("%p", line);
- 		free (line);
-		i++;
-		system("leaks a.out");
- 	}
- 	close(fd);
- 	return (0);
- }*/
-
-
 char *get_next_line(int fd)
 {
 	int rd;
@@ -109,3 +46,26 @@ char *get_next_line(int fd)
 	line[len_line] = 0;
 	return (line);
 } 
+
+/*
+ int main()
+ {	
+
+ 	int fd;
+ 	char	*line;
+	int i = 0;
+
+ 	fd = open("test.txt", O_RDONLY);
+ 	while (i>=0)
+ 	{
+ 		line = get_next_line(fd);
+ 		if (line == NULL)
+ 			break;
+ 		printf("%p", line);
+ 		free (line);
+		i++;
+		system("leaks a.out");
+ 	}
+ 	close(fd);
+ 	return (0);
+ }*/
